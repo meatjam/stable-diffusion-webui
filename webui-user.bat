c1e587fe17565bc5fe5a81d98b02efea06892cc6
meatjam <851404658@qq.com>
2023-02-25 23:27:26 +0800

    add xformers

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=venv\Scripts\python.exe
 set GIT=
 set VENV_DIR=venv
-set COMMANDLINE_ARGS=--api --disable-nan-check
+set COMMANDLINE_ARGS=--api --disable-nan-check --xformers
 
 call webui.bat
